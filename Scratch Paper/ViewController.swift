c6d5de83d97ea5083510f4d27e705447fb32ac9e
CHENGJUN LU <chengjunlu@chengjuns-macbook-pro.local>
2018-07-01 16:46:34 -0700

    Initial Commit

---
+++ Scratch Paper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scratch Paper
+//
+//  Created by CHENGJUN LU on 7/1/18.
+//  Copyright © 2018 CHENGJUN LU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
